3cb1ad663dc75d87fccecf58343b7d7b278e8421
carozh91 <165485810+carozh91@users.noreply.github.com>
2025-08-26 12:08:28 -0500

    Update app_final2.py

--- app_final2.py
+++ app_final2.py
@@ -839,11 +839,12 @@ with tab_procesamiento:
             store_csv_in_memory(df_resumen_db, csv_name)
 
             st.info("📁 Resumen guardado temporalmente en memoria. Usa la pestaña '💾 Guardar información' para persistir el proyecto.")
-        nav_buttons("📝 Ingreso de información", "📈 Comparativos")
+        
 
 
             cursor.close()
             conn.close()
+    nav_buttons("📝 Ingreso de información", "📈 Comparativos")
 
 # 📈 COMPARATIVOS
 with tab_comparativos:
